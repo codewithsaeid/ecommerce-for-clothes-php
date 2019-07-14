faaaa618d8807d0c1a69746fa2210e6ced1f8d80
muhammadsaeid0 <muhammadsaeid0@gmail.com>
2019-07-15 05:03:19 +0600

    database update

--- Database/ecommercestore.sql
+++ Database/ecommercestore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2019 at 05:34 PM
+-- Generation Time: Jul 15, 2019 at 01:02 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -25,35 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `add_product`
---
-
-CREATE TABLE `add_product` (
-  `pid` int(250) NOT NULL,
-  `p_name` varchar(250) NOT NULL,
-  `p_code` varchar(250) NOT NULL,
-  `p_category` varchar(250) NOT NULL,
-  `p_price` varchar(250) NOT NULL,
-  `p_img` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `add_product`
---
-
-INSERT INTO `add_product` (`pid`, `p_name`, `p_code`, `p_category`, `p_price`, `p_img`) VALUES
-(4, 'Shirt', 'P12', 'Man', '$30', '12.jpg'),
-(5, 'Shirt', 'P13', 'Man', '$30', '13.jpg'),
-(6, 'L.Shirt', 'P14', 'Women', '$30', '15.jpg'),
-(8, 'L shirt', '320BC', 'Man', '234', '16.jpg'),
-(15, 'x-man', '007', 'Man', '40', '14064049_176753079400705_617919488227609300_n.jpg'),
-(16, 'x-man', '007', 'women', '40', '14064049_176753079400705_617919488227609300_n.jpg'),
-(17, 'x-man', '007', 'kids', '40', '14064049_176753079400705_617919488227609300_n.jpg'),
-(18, 'x-man', '007', 'accessories', '40', '14064049_176753079400705_617919488227609300_n.jpg');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `admin_info`
 --
 
@@ -70,15 +41,56 @@ CREATE TABLE `admin_info` (
 INSERT INTO `admin_info` (`id`, `admin_email`, `admin_pass`) VALUES
 (1, 'admin@gmail.com', 'admin@gmail.com');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `pid` int(250) NOT NULL,
+  `p_name` varchar(250) NOT NULL,
+  `p_code` varchar(250) NOT NULL,
+  `p_category` varchar(250) NOT NULL,
+  `p_price` varchar(250) NOT NULL,
+  `p_img` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`pid`, `p_name`, `p_code`, `p_category`, `p_price`, `p_img`) VALUES
+(15, 'saeidasdf', '222', 'kids', '222', 'diana-penty-in-cocktail-movie-14.jpg'),
+(16, 'x-man', '007', 'women', '40', '14064049_176753079400705_617919488227609300_n.jpg'),
+(17, 'x-man', '007', 'kids', '40', '14064049_176753079400705_617919488227609300_n.jpg'),
+(18, 'x-man', '007', 'accessories', '40', '14064049_176753079400705_617919488227609300_n.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `id` int(100) NOT NULL,
+  `user_name` varchar(250) NOT NULL,
+  `user_email` varchar(250) NOT NULL,
+  `pass` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`id`, `user_name`, `user_email`, `pass`) VALUES
+(1, 'saeid', 'saeid@gmail.com', 'asdf'),
+(2, 'saeid', 'saeid@gmail.com', 'asdf'),
+(3, 'Shafiur', 'shafiur@gmail.com', 'asdf');
+
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `add_product`
---
-ALTER TABLE `add_product`
-  ADD PRIMARY KEY (`pid`);
 
 --
 -- Indexes for table `admin_info`
@@ -87,20 +99,38 @@ ALTER TABLE `admin_info`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `products`
 --
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`pid`);
 
 --
--- AUTO_INCREMENT for table `add_product`
+-- Indexes for table `user_info`
 --
-ALTER TABLE `add_product`
-  MODIFY `pid` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
 --
 -- AUTO_INCREMENT for table `admin_info`
 --
 ALTER TABLE `admin_info`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `pid` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
